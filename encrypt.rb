b14927b7c891c265888a8b43b58c0a0550ff53f5
lordsynergy <djordj88@mail.ru>
2021-09-05 16:22:14 +0300

    SHA2 add

--- encrypt.rb
+++ encrypt.rb
@@ -8,11 +8,13 @@ user_input = STDIN.gets.chomp
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 user_choice = STDIN.gets.to_i
 
 puts "Вот что получилось:"
 case user_choice
 when 1 then puts Digest::MD5.hexdigest(user_input)
 when 2 then puts Digest::SHA1.hexdigest(user_input)
+when 3 then puts Digest::SHA2.hexdigest(user_input)
 else puts user_input
 end
